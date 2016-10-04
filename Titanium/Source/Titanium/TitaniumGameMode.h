ae01fefdeabac4ba8d5e6efc1810a1f7a93c9928
Christian Holt <sidaroth@protonmail.com>
2016-10-04 15:17:41 +0200

    Initial commit. Project setup - #1

---
+++ Titanium/Source/Titanium/TitaniumGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TitaniumGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TITANIUM_API ATitaniumGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
